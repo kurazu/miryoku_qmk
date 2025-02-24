7c5a5a557fa590ec71fbbcbfd638a3d02fa5c4a6
Tomasz Maćkowiak <kurazu@kurazu.net>
2025-02-24 10:34:25 +0100

    prev config

--- users/manna-harbour_miryoku/config.h
+++ users/manna-harbour_miryoku/config.h
@@ -9,7 +9,7 @@
 
 // default but used in macros
 #undef TAPPING_TERM
-#define TAPPING_TERM 500
+#define TAPPING_TERM 300
 
 // Prevent normal rollover on alphas from accidentally triggering mods.
 #define IGNORE_MOD_TAP_INTERRUPT
